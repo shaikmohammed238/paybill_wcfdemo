8dce9754279008580d70c63db972c543b990612b
SHAIK MOHAMMED GHOUSE <SHAIKGHOUSE6268@GMAIL.COM>
2022-04-08 13:12:00 +0530

    added demo wcf all api project

---
+++ Employee/IEmpService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Employee
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmpService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmpService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/EmpPayBill/{EmpPayId}", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string EmpPayBill(string EmpPayId);
+    }
+}
